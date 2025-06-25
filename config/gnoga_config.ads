b0ddd1e5992eec05b035ad6fa50e0825e68ffcee
wbullaughey <wlb122@verizon.net>
2025-06-25 11:06:05 -0400

    merge to laptop 6/25/25

--- config/gnoga_config.ads
+++ config/gnoga_config.ads
@@ -15,6 +15,6 @@ package Gnoga_Config is
    Alire_Host_Distro : constant String := "homebrew";
 
    type Build_Profile_Kind is (release, validation, development);
-   Build_Profile : constant Build_Profile_Kind := release;
+   Build_Profile : constant Build_Profile_Kind := development;
 
 end Gnoga_Config;
